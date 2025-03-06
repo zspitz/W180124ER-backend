4771d6b5d2c2b8111c045e06b8136db303afd42b
Zev Spitz <shivisi@hotmail.com>
2025-03-06 19:20:03 +0200

    ESLint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-const s: string = "abcd";
-console.log(s);
-console.log(s + s);
+const s: string = 'ab"cd'
+const s1 = 'xyz'
+console.log(s)
+console.log(s + s)
+
+console.log(`print ${s}`)
+
+const fn = () => {
+    console.log(5)
+}
