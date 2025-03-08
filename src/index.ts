e84c17264bbfda5d4aa956ac1c9729d033aeb539
Zev Spitz <shivisi@hotmail.com>
2025-03-08 22:48:46 +0200

    Replace dotenv with scripts & command-line args

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-console.log(process.env.NODE_ENV)
-
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
+console.log(process.env.PORT)
 
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send(`Hello ${process.env.HELLO || ''}`)
